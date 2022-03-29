8e7e8b41cbd6090c5aa732cf0f96b19a8a730faf
Aravind Reddy V <zerodividedby0@gmail.com>
2022-03-29 13:36:23 +0530

    Stop CSI before disabling

--- picsi/commands/disable.py
+++ picsi/commands/disable.py
@@ -2,10 +2,11 @@ __all__ = ["disable"]
 
 from pathlib import Path
 from halo import Halo
-
+from typer import Exit
 from picsi.vendored.run_commands import run_commands
 from picsi.vendored.get_uname import get_uname
 from picsi.vendored.get_brcmfmacko import get_brcmfmacko
+from picsi.commands.down import down as cmd_down
 
 
 def disable():
@@ -13,32 +14,46 @@ def disable():
     Disable CSI collection
     """
 
+    # Check if picsi has been installed
+    state_picsi_is_installed = Path("/home/pi/.picsi/state/picsi_is_installed")
+    state_picsi_is_installed.parent.mkdir(exist_ok=True, parents=True)
+
+    if not state_picsi_is_installed.is_file():
+        print("Error. Have you run `picsi install`?")
+        raise Exit(10)
+
+    # Check if firmware is already disabled.
+    state_firmware_is_patched = Path("/home/pi/.picsi/state/firmware_is_patched")
+    state_firmware_is_patched.parent.mkdir(exist_ok=True, parents=True)
+
+    if not state_firmware_is_patched.is_file():
+        print("Firmware is already disabled.")
+        return
+
+    # Stop CSI collection if it's running.
+    state_csicollection_is_up = Path("/var/run/picsi/state/csicollection_is_up")
+    state_csicollection_is_up.parent.mkdir(exist_ok=True, parents=True)
+
+    if state_csicollection_is_up.is_file():
+        cmd_down()
+
     with Halo(spinner="dots") as spinner:
+        spinner.text = "Restoring original firmware"
 
-        Path("/home/pi/.picsi/state").mkdir(exist_ok=True, parents=True)
-
-        path_picsi_up = Path("/home/pi/.picsi/state/firmware_is_up")
         path_nexmon_csi_bin = Path(f"/home/pi/.picsi/bins/{get_uname('-r')}")
         path_brcmfmacko: Path = get_brcmfmacko()
 
-        if not path_picsi_up.is_file():
-            return
-
-        path_picsi_up.unlink()
-
-        # Enable wpa_supplicant
-        spinner.text = "Enabling wpa_supplicant"
-
+        # Unblock WPA_supplicant on wlan0
         with open("/etc/dhcpcd.conf", "r") as ifile:
-            dhcpcd_conf = ifile.read().replace(
-                "\ndenyinterfaces wlan0\ninterface wlan0\n\tnohook wpa_supplicant\n", ""
-            )
+            dhcpcd_conf = ifile.read()
 
         with open("/etc/dhcpcd.conf", "w") as ofile:
-            ofile.write(dhcpcd_conf)
-
-        # TODO: Investigate disabling CSI collection
-        # by stopping nexutil
+            ofile.write(
+                dhcpcd_conf.replace(
+                    "\ndenyinterfaces wlan0\ninterface wlan0\n\tnohook wpa_supplicant\n",
+                    "",
+                )
+            )
 
         # fmt: off
         run_commands([
@@ -50,15 +65,15 @@ def disable():
             ["insmod", path_brcmfmacko],
             ["depmod", "-a"],
 
+            "# Enabling wpa_supplicant on wlan0",
+            ["systemctl", "enable", "--now", "wpa_supplicant"],
+            ["wpa_supplicant", "-B", "-c", "/etc/wpa_supplicant/wpa_supplicant.conf", "-i", "wlan0"],
+            ["dhcpcd", "wlan0"],
+
             "# Restarting WiFi",
             ["ip", "link", "set", "dev", "wlan0", "down"],
             ["ip", "link", "set", "dev", "wlan0", "up"],
-
-            "# Enabling wpa_supplicant",
-            ["systemctl", "enable", "--now", "wpa_supplicant"],
-            ["wpa_supplicant", "-B", "-c", "/etc/wpa_supplicant/wpa_supplicant.conf", "-i", "wlan0"],
-            ["dhcpcd", "wlp2s0"]
         ], spinner, log_title='cmd-disable')
         # fmt: on
 
-        print("Done. If WiFi connectivity is not restored, try rebooting.")
+        state_firmware_is_patched.unlink()
